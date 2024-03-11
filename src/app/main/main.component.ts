15fa57989dba5f15c7191f0763e7fd757aab3118
Ogunlade Adebayo Peter <144332360+Adebayozz1994@users.noreply.github.com>
2024-03-11 20:51:56 -0100

    new commit

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.css'
+})
+export class MainComponent {
+  imageUrl: string = './assets/image/dxf91zhqd2z6b0bwg85ktm5s4.svg';
+}
